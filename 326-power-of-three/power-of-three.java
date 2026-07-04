5c9a461aed3b66626c707041614d63a29d0b367c
Anmol Agarwal <agarwal.anmol6306@gmail.com>
2026-07-04 12:41:18 +0530

    Time: 8 ms (91.48%) | Memory: 45.9 MB (65.36%) - LeetSync

---
+++ 326-power-of-three/power-of-three.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else{
+            while(n%3==0){
+                n/=3;
+            }
+            if(n==1){
+                return true;
+            }else return false;
+        }
+        
+    }
+}

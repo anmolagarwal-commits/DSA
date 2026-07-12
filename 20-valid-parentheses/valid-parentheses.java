0afadfdc3105a0e8ff3f12a2726f908455681910
Anmol Agarwal <agarwal.anmol6306@gmail.com>
2026-07-12 22:42:44 +0530

    Time: 4 ms (38.38%) | Memory: 43.9 MB (6.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)== '[') {
+                st.push(s.charAt(i));
+            }else{
+                if (st.isEmpty()) 
+                {
+                    return false;
+                }
+                if ((s.charAt(i) == ')'&& st.peek()!='(')||(s.charAt(i) == ']'&& st.peek()!='[')||(s.charAt(i) =='}'&& st.peek()!= '{')){
+                    return false;}
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
